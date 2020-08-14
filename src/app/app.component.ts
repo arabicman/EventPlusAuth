f04e8b15ed3d4946abbd6fc9c29275f035e12529
Napoleon Li <lijx@LJX-Mac.local>
2020-08-13 21:54:08 -0700

    dasdsa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngApp';
+  constructor(private _authService:AuthService){}
 }
